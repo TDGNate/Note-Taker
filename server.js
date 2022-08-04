62d214cb2da70ea27c01a6d69c2e13c03219a18d
Nate <itsnzte@gmail.com>
2022-08-03 23:04:31 -0700

    add delete button, so the user can now delete certain notes. And changed up my res status to chain on the send method.

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ const fs = require('fs');
 const uniqid = require('uniqid'); 
 
 // Middleware 
-app.use(express.json())
-app.use(express.static('public'))
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 // Get method when the url is to the root
 app.get('/', (req, res) => {
@@ -32,7 +33,6 @@ app.get('/api/notes', (req, res) => {
 
       // change Data into an Obj 
       const objData = JSON.parse(data)
-      console.log(objData)
       res.json(objData)
     }
   })
@@ -45,7 +45,8 @@ app.post('/api/notes', (req, res) => {
   const title = req.body.title;
   const text = req.body.text;
 
-  res.send(200, { title, text })
+  // sending back a status 200 
+  res.status(200).send({ title, text })
 
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) {
@@ -59,16 +60,50 @@ app.post('/api/notes', (req, res) => {
         text,
         id: uniqid()
       }
+
+      // pushing user's new note into variable database 
       notesObj.push(newNote)
-      console.log(`It has been written! \n ${notesObj}`)
+      // console.log(`It has been written! \n ${notesObj}`)
 
       fs.writeFile('./db/db.json', JSON.stringify(notesObj), err => { err ? console.log(err) : console.log('Database Updated!') })
     }
   })
 })
 
+// delete method for api to notes url 
+app.delete('/api/notes/:id', (req, res) => {
+  let id = req.params.id
+
+  fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+    if (err) {
+      console.log(err)
+    } else {
+      const dataBase = JSON.parse(data);
+
+      // loop through database to match IDs to delete 
+      for (i = 0; i < dataBase.length; i++) {
+        if (dataBase[i].id === id) {
+          dataBase.splice(i, 1);
+
+        // Update the database with deleted note
+        fs.writeFile('./db/db.json', JSON.stringify(dataBase), err => {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log('Deleted Note in Database!')
+            return res.send("Deleted!");
+          }
+        })
+        }
+      }
+    }
+  })
+})
+
+// Checks if the user goes to a different URL to redirect them to the index.html 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
+// listening to port when server starts 
 app.listen(PORT, () => console.log(`Listening on PORT http://localhost:${PORT} 🐸`))
